22d25ebab9084c216ae18135f6a7043d8c690672
Abhinav Subramani <abhisubramani3@gmail.com>
2022-06-22 23:43:19 -0400

    Squares for grid

--- minesweeper.js
+++ minesweeper.js
@@ -1,10 +1,28 @@
-// 1. Make a grid
+// 1. Make a grid with all hidden tiles
+// 2. Mark 10 random tiles with mines
 
-// When page loads up, run the following function
+var numCols = 10;
+var numRows = 10;
+
+var numMines = 10;
+
+// When page loads up, run the following function to load up all the hidden tiles
 window.onload = function() {
+    gameSetup();
+};
 
-}
-
-function gameStart() {
-    
-}
+// learn about document object: https://www.w3schools.com/jsref/dom_obj_document.asp
+function gameSetup() {
+    rowList = [];
+    for (let r=0; r<numRows; r++) {
+        colList = [];
+        for (let c=0; c<numCols; c++) {
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            document.getElementById("board").append(tile);
+            colList.push(tile);
+        };
+        rowList.push(colList);
+    }
+    console.log(rowList);
+};
